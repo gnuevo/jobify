7a821d284f78bec959ba1914a348a40ded6d734d
Gregorio Nuevo <gncdvp@gmail.com>
2019-03-03 19:52:53 +0100

    Add sample usage of superagent to query the server to background script

--- background.js
+++ background.js
@@ -1,4 +1,43 @@
+const superagent = require('superagent');
+
+// superagent.get('https://api.nasa.gov/planetary/apod')
+// .query({ api_key: 'DEMO_KEY', date: '2017-08-02' })
+// .end((err, res) => {
+//   if (err) { return console.log(err); }
+//   console.log(res.body.url);
+//   console.log(res.body.explanation);
+// });
+
+// superagent.get('https://reqres.in/api/users')
+// .query({ page: 2, per_page: 5 })
+// .end((err, res) => {
+//   if (err) { return console.log(err); }
+//   console.log(res);
+//   console.log(res.body);
+// });
+
+superagent.post('http://localhost:3000/jobs')
+// .withCredentials()
+.send({
+      id: "glassdoor:new_job",
+      platform: "glassdoor",
+      jobUrl: "<url>",
+      title: "Cool job:new_job",
+      company: "Cool company:new_job",
+      description: "Oh, this job is really cool:new_job"
+    })
+.then(res => {
+  console.log("Rersponse from server:");
+  console.log(res);
+  console.log(res.body);
+})
+.catch(err => {
+  console.log("Error from server:");
+  console.log(err);
+});
+
 console.log("background.js");
+
 
 // browser.sidebarAction.setPanel(
 //     {
